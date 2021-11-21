41463b992859a77e374c1df8730e805fc79d9853
JeongInPark <nickcaestro@gmail.com>
2021-11-21 22:45:49 +0900

    Deistini Challenge Start

--- destini-challenge-starting/lib/main.dart
+++ destini-challenge-starting/lib/main.dart
@@ -77,21 +77,23 @@ class _StoryPageState extends State<StoryPage> {
                 // from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
                   visible: storyBrain.buttonShouldBeVisible(),
-                  child: FlatButton(
-                    onPressed: () {
-                      //Choice 2 made by user.
-                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                      setState(() {
-                        storyBrain.nextStory(2);
-                      });
-                    },
-                    color: Colors.blue,
-                    child: Text(
-                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                      //'Choice 2',
-                      storyBrain.getChoice2(),
-                      style: TextStyle(
-                        fontSize: 20.0,
+                  child: Visibility(
+                    child: FlatButton(
+                      onPressed: () {
+                        //Choice 2 made by user.
+                        //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                        setState(() {
+                          storyBrain.nextStory(2);
+                        });
+                      },
+                      color: Colors.blue,
+                      child: Text(
+                        //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                        //'Choice 2',
+                        storyBrain.getChoice2(),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                   ),
